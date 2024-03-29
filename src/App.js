1af291103f33112eddb966b919a7f041054f42e9
Igor Prus <igorprus42@gmail.com>
2024-03-29 18:09:24 +0100

    added previous and next button, active and disabled numbers

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ const App = () => {
         tagsPerPage={tagsPerPage}
         totalTags={tags.length}
         paginate={paginate}
+        currentPage={currentPage}
       />
     </div>
   );
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,25 +1,56 @@
 import React from "react";
 
-const Pagination = ({ tagsPerPage, totalTags, paginate }) => {
-    const pageNumbers = [];
-    
-    for(let i = 1; i <= Math.ceil(totalTags / tagsPerPage); i++){
-        pageNumbers.push(i);
-    }
+const Pagination = ({ tagsPerPage, totalTags, paginate, currentPage }) => {
+  const pageNumbers = [];
 
-    return (
-        <div>
-        <ul className="pagination">
-        {pageNumbers.map(number => (
-            <li key={number} className="page-item">
-                <a onClick={() => paginate(number)} href="#" className="page-link">
-                    {number}
-                </a>
-            </li>
+  for (let i = 1; i <= Math.ceil(totalTags / tagsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div>
+      <ul className="pagination">
+        <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
+          <a
+            className="page-link"
+            href="#"
+            aria-label="Previous"
+            onClick={() => paginate(currentPage - 1)}
+          >
+            <span aria-hidden="true" tabindex="1">
+              &laquo;
+            </span>
+            <span class="visually-hidden">Previous</span>
+          </a>
+        </li>
+        {pageNumbers.map((number) => (
+          <li
+            key={number}
+            className={`page-item ${currentPage === number ? "active" : ""}`}
+          >
+            <a onClick={() => paginate(number)} href="#" className="page-link">
+              {number}
+            </a>
+          </li>
         ))}
-        </ul>
-        </div>
-    )
-}
+        <li className="page-item">
+          <a
+            className={`page-link ${
+              currentPage === Math.ceil(totalTags / tagsPerPage)
+                ? "disabled"
+                : ""
+            }`}
+            href="#"
+            aria-label="Next"
+            onClick={() => paginate(currentPage + 1)}
+          >
+            <span aria-hidden="true">&raquo;</span>
+            <span className="visually-hidden">Next</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Pagination
+export default Pagination;
